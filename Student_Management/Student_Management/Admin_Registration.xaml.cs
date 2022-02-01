74d508e17174e7975ae1ba7847391820d69b96ad
Suvrodev <suvrodevhowlader1408@gmail.com>
2022-02-02 01:40:09 +0600

    admin_Registration

---
+++ Student_Management/Student_Management/Admin_Registration.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Student_Management
+{
+    /// <summary>
+    /// Interaction logic for Admin_Registration.xaml
+    /// </summary>
+    public partial class Admin_Registration : Window
+    {
+        public Admin_Registration()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Reset_btn(object sender, RoutedEventArgs e)
+        {
+            FName_Box.Text = "";
+            LName_Box.Text="";
+            Age_box.Text = ""; 
+            mail_box.Text = "";
+            Phn_Box.Text = "";
+            Loc_box.Text = "";
+            Skill_Box.Text = "";
+            Pass_box.Password = "";
+            CPass_box.Password = "";
+            
+        }
+
+        private void Reg_Click_btn(object sender, RoutedEventArgs e)
+        {
+            String FName = FName_Box.Text;
+            String LName = LName_Box.Text;
+            String Age = Age_box.Text;
+            String Email = mail_box.Text;
+            String Phone = Phn_Box.Text;
+            String Location = Loc_box.Text;
+            String Skill = Skill_Box.Text;
+            String Password = Pass_box.Password;
+            String Confirm_Password = CPass_box.Password;
+            String Sex="", Merital_Status="";
+
+            if (RB_Male.IsChecked == true)
+            {
+                Sex = "Male";
+            }
+            else if (RB_Female.IsChecked == true)
+            {
+                Sex = "Female";
+            }
+            else if (RB_Other.IsChecked == true)
+            {
+                Sex = "other";
+            }
+            var Religion_Temp = (ComboBoxItem)comboBox_rl.SelectedItem;
+            String Religion = (String)Religion_Temp.Content;
+     
+
+            if (Merried.IsChecked == true)
+            {
+                Merital_Status = "Merried";
+            }
+            if (Unmerried.IsChecked == true)
+            {
+                Merital_Status = "Unmerried";
+            }
+
+            string DOB = cal_date.SelectedDate.Value.ToShortDateString();
+         
+            //Message box for surity of data
+            MessageBoxResult results = MessageBox.Show(FName+"\n"+LName + "\n" + Age + "\n" + Sex + "\n" + Religion + "\n" + Merital_Status + "\n" + Email + "\n" + Phone
+                + "\n" + DOB + "\n" + Location + "\n" + Skill + "\n" + Password + "\n" + Confirm_Password);
+
+
+            if (Password.Length >= 6)
+            {
+                if (Password == Confirm_Password)
+                {
+                    //Message box for surity of insert
+                    MessageBoxResult m = MessageBox.Show("Do you relly Insert?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                    switch (m)
+                    {
+                        //1st case
+                        case MessageBoxResult.Yes:
+                            /////Insert Data////
+
+
+                            String Connection = "Server=127.0.0.1;User ID=root; DataBase=project";
+                            String Query = "INSERT INTO `admin`(`fname`, `lname`, `age`, `sex`, `Religion`, `m_s`, `mail`, `phn`, `dob`, `loc`, `skill`, `pass`, `status`) VALUES ('" + FName + "','" + LName + "','" + Age + "','" + Sex + "','" + Religion + "','" + Merital_Status + "','" + Email + "','" + Phone + "','" + DOB + "','" + Location + "','" + Skill + "','" + Password + "','False')";
+
+
+
+                            MySqlConnection mycon = new MySqlConnection(Connection);
+                            MySqlCommand myCom = new MySqlCommand(Query, mycon);
+                            mycon.Open();
+                            MySqlDataReader reader = myCom.ExecuteReader(); ;
+                            mycon.Close();
+                            MessageBoxResult result = MessageBox.Show("Data Inserted Successfully");
+
+
+                            ///Insert Data Close///
+
+                            break;
+
+                        //2nd Case
+                        case MessageBoxResult.No:
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBoxResult result_ = MessageBox.Show("Password Not Matching");
+                }
+            }
+            else
+            {
+                MessageBoxResult result_ = MessageBox.Show("Password should be more than 5 digit");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Student_Management/Student_Management/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Student_Management
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click_btn(object sender, RoutedEventArgs e)
+        {
+            String ID = ID_Box.Text;
+            String Password = Pass_Box.Password;
+
+            if(ID=="abc" && Password == "123")
+            {
+                After_Login a = new After_Login();
+                a.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Wrong ID or Password");
+            }
+        }
+
+        private void Reset_btn(object sender, RoutedEventArgs e)
+        {
+            //String ID, Password;
+           ID_Box.Text="";
+           Pass_Box.Password="";
+
+        }
+
+
+        private void ID_Box_focus(object sender, RoutedEventArgs e)
+        {
+            ID_Box.Text = "";
+        }
+
+
+        private void Registration_btn_click(object sender, RoutedEventArgs e)
+        {
+            Admin_Registration a = new Admin_Registration();
+            a.Show();
+            this.Close();
+        }
+    }
+}
